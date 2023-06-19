33a561d624cf595ed6219c64bbb9a7fce3d440fc
Reinhard von Lohengramm <bride.r@northeastern.edu>
2023-06-18 23:36:22 -0400

    Finished Vectors

--- HDL_Bits_Vectors.sv
+++ HDL_Bits_Vectors.sv
@@ -51,6 +51,47 @@ module _gates4 (
 
         assign out_and = ( in[0] & in[1] & in[2] & in[3] );
         assign out_or = ( in[0] | in[1] | in[2] | in[3] );
+        assign out_xor = ( in[0] ^ in[1] ^ in[2] ^ in[3] );
         
+endmodule
+
+module _vector_reversal (
+        input [7:0] in,
+        output [7:0] out
+        );
+
+        //Assigning out[7:0] = in[0:7] doesn't work because verilog doesn't allow vector orders to be flipped
+        assign out [7:0] = { in[0], in[1], in[2], in[3], in[4], in[5], in[6], in[7] };
+        
+endmodule
+
+//Replicate vector 
+/*
+{5{1'b1}}           // 5'b11111 (or 5'd31 or 5'h1f)
+{2{a,b,c}}          // The same as {a,b,c,a,b,c}
+{3'd5, {2{3'd6}}}   // 9'b101_110_110. It's a concatenation of 101 with
+                    // the second vector, which is two copies of 3'b110.
+
+Build a circuit that sign-extends an 8-bit number to 32 bits. 
+This requires a concatenation of 24 copies of the sign bit (i.e., replicate bit[7] 24 times) followed by the 8-bit number itself.
+*/
+module _Vector4 (
+        input [7:0] in,
+        output [31:0] out
+        );
+
+        // Have to put the replication portion into the {} or it fails lmaooo
+        assign out = { {24{in[7]}}, in };
+endmodule
+
+`default_nettype none
+//Do again
+module _Vector5(
+        input a, b, c, d, e,
+        output [24:0] out
+        );
+
+        assign out = ~{ { {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} } ^ { 5{a, b, c, d, e} } };
 
 endmodule
+//If you attach them to wires for some reason it doesn't work
