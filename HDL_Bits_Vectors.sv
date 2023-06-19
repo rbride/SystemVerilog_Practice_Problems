499b2566eda0b32f8357da11294eb0afe117e841
Reinhard von Lohengramm <bride.r@northeastern.edu>
2023-06-18 21:57:37 -0400

    Created File and did first problem of the vector section

---
+++ HDL_Bits_Vectors.sv
@@ -0,0 +1,12 @@
+module _Vector0 ( 
+        input wier [2:0] vec,
+        output wire [2:0] outv,
+        output wire o0, o1, o2 
+        );  
+
+    assign outv = vec;
+    assign o2 = vec[2];
+    assign o1 = vec[1];
+    assign o0 = vec[0];
+
+endmodule
