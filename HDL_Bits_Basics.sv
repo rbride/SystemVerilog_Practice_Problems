9d95047bc1f4e12ea09be824e3fc76e51b35e8db
Reinhard von Lohengramm <bride.r@northeastern.edu>
2023-06-18 21:51:48 -0400

    Finished basic Problems Moving on to Vectors

--- HDL_Bits_Basics.sv
+++ HDL_Bits_Basics.sv
@@ -10,6 +10,8 @@ in1     in2     out         in1     in2     out         in1     in2     out
 
 Memorize the operators
 https://class.ece.uw.edu/cadta/verilog/operators.html
+memorize symbols too
+https://www.bing.com/images/search?view=detailV2&ccid=gjm0yCBi&id=CFA2399235312FB0C0ED6EC34E192D2B5591BD20&thid=OIP.gjm0yCBiOP2eeOXO4Lh8JAHaFm&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.8239b4c8206238fd9e78e5cee0b87c24%3frik%3dIL2RVSstGU7Dbg%26riu%3dhttp%253a%252f%252fegomachines.com%252fanimoid%252fann-hardware%252fgates.png%26ehk%3dBrbAkOUUgHJN5GNjTiQU6tLxu4IwnJTlkKsReHvA%252b4U%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=443&expw=585&q=Circuit+gate+symbols&simid=608034436101914028&FORM=IRPRST&ck=9FC1920E177C9956F062E67936374AAF&selectedIndex=0&ajaxhist=0&ajaxserp=0
 */
 
 //Wire: Create a module that behaves like a wire
@@ -72,3 +74,33 @@ module _xnor_alt ( input wire a, b, output wire out );
     assign out = ( a ~^ b ) ? 1'b1 : 1'b0;
 endmodule
 //Both succeed
+
+// Declaring Wires: implement the described circuit
+`default_nettype none
+module _decl_wires ( input a, b c, d, output out, out_n );
+    wire x, y, z; //X y are the frist two, z is the "one wire"
+
+    assign x = ( a & b ) ? 1'b1 : 1'b0;
+    assign y = ( c & d ) ? 1'b1 : 1'b0;
+    assign out = ( x | y ) ? 1'b1 : 1'b0;
+    assign out_n = ~out;
+
+endmodule 
+
+// 7458 Chip, I.e four ands two ors, slighlyt more complex than 7420
+module _7458 ( 
+                input p1a, p1b, p1c, p1d, p1e, p1f, p2a, p2b, p2d,
+                output p1y, p2y
+             );
+
+    wire a, b, c, d;
+
+    assign a = ( p1c & p1b & p1a ) ? 1'b1 : 1'b0;
+    assign b = ( p1f & p1e & p1d ) ? 1'b1 : 1'b0;
+    // I don't have to do any of the conditonals lmao, I am going to keep doing so for practice typing them
+    assign p1y = ( a | b ) ? 1'b1 : 1'b0;
+    assign c = ( p2a & p2b ) ? 1'b1 : 1'b0;
+    assign d = ( p2c & p2d ) ? 1'b1 : 1'b0;
+    assign p2y = ( c | d );
+
+endmodule 
