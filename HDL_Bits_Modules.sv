cb4434606c8d8bb0e23a298468be3cf6f1b113cc
Reinhard von Lohengramm <bride.r@northeastern.edu>
2023-07-04 21:44:20 -0400

    another day another problem

--- HDL_Bits_Modules.sv
+++ HDL_Bits_Modules.sv
@@ -118,3 +118,40 @@ module add1 (
     end
     
 endmodule
+
+//Carry select adder
+/// BRUUUUUUUUHHHHHHHH  memorize the syntax for case switches that was bad
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+
+    wire cout_lower;
+    wire [15:0] upper16_out1;
+    wire [15:0] upper16_out2;
+
+    add16 _lower16 ( .a(a[15:0]), .b(b[15:0]), .sum(sum[15:0]), .cout(cout_lower));
+    add16 _upper16_1 ( .a(a[31:16]), .b( b[31:16]), .sum( upper16_out1[15:0] ), .cin(1'b0));
+    add16 _upper16_2 ( .a(a[31:16]), .b( b[31:16]), .sum( upper16_out2[15:0] ), .cin(1'b1));
+
+    always @* begin
+        case (cout_lower) 
+            1'b0    :   sum[31:16] = upper16_out1;
+            1'b1    :   sum[31:16] = upper16_out2;
+        endcase
+    end
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
